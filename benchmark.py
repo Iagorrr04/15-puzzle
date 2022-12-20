3fc4d8eda2c4b007c4c1bac4e572bdb9f66ca387
iagorrr04 <iagorrr04@gmail.com>
2022-12-20 12:08:31 -0300

    benchmark pra rodar na choco.

---
+++ benchmark.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+size = '3x3'
+for input in os.listdir(f'input/samples/{size}'):
+    print(f"Trying: {input}")
+    os.system(f'touch benchmark/{size}/{input}')
+    
+    start = time.perf_counter()
+    os.system(f'./a.out < input/samples/{size}/{input} > benchmark/{size}/{input}')
+    end = time.perf_counter()
+    
+    print(end-start)
+    path = f'benchmark/{size}/{input}'
+    with open(path , 'a') as f:
+        f.write(str(end-start)+'\n')
+        print(f)
+    
